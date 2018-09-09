0a2de65beb1614b4d510ec55013084121532808b
Saurav Sengupta <sauravsengupta7@gmail.com>
2018-09-09 14:53:01 -0400

    linear model attempt final

--- regression_Saurav.R
+++ regression_Saurav.R
@@ -17,10 +17,10 @@ rate_levels <- 1:10
 house_prices$MSSubClass <- factor(house_prices$MSSubClass)
 house_prices$OverallQual <- factor(house_prices$OverallQual, levels = rate_levels)
 house_prices$OverallCond <- factor(house_prices$OverallCond, levels = rate_levels)
-house_prices$YearBuilt <- factor(house_prices$YearBuilt)
-house_prices$YearRemodAdd <- factor(house_prices$YearRemodAdd)
+# house_prices$YearBuilt <- factor(house_prices$YearBuilt)
+# house_prices$YearRemodAdd <- factor(house_prices$YearRemodAdd)
 house_prices$MoSold <- factor(house_prices$MoSold)
-house_prices$YrSold <- factor(house_prices$YrSold)
+# house_prices$YrSold <- factor(house_prices$YrSold)
 
 # replace missing values in factors as NA string and integers as imputed values
 cols <- colnames(house_prices)
@@ -29,17 +29,18 @@ for (i in 1:ncol(house_prices)){
         df_col <- house_prices[,cols[i]]
         if(is.factor(df_col)){
                 # print(i)
-                levels <- levels(df_col)
+                # levels <- levels(df_col)
                 level_df <- table(df_col) %>% as.data.frame()
                 level_df <- level_df[order(-level_df$Freq),]
                 imputed_level <- level_df[1,1]
                 df_col[is.na(df_col)] <- imputed_level
         }
         else {
-                df_col[is.na(df_col)] = mean(df_col, na.rm = TRUE)
+                df_col[is.na(df_col)] = mean(df_col,na.rm = TRUE)
         }
         house_prices[,cols[i]] <- df_col
 }
+
 
 str(house_prices)
 
@@ -49,6 +50,8 @@ inTrain <- createDataPartition(y=house_prices$SalePrice,
 training <- house_prices[inTrain, ]
 validation <- house_prices[-inTrain, ]
 
+validation[,a] <- factor(df_test[,a], levels = levels(df_train[,a]))
+
 pr.lm1 <- lm(SalePrice ~ ., data = training)
 
 
@@ -57,16 +60,10 @@ options(max.print = 2500)
 
 summary(pr.lm1)
 
-pr.lm2 <- lm(SalePrice ~ MSSubClass + LotArea + MSZoning + 
-                     LotConfig + LandSlope + Neighborhood +
-                     Condition1  + HouseStyle + OverallQual +
-                     YearRemodAdd + RoofMatl + MasVnrArea + BsmtQual +
-                     BsmtExposure + BsmtFinType1 + BsmtFinSF1 + BsmtFinSF2 + BsmtUnfSF +
-                     X1stFlrSF + X2ndFlrSF + ScreenPorch +PoolArea + PoolQC +
-                     MiscFeature, data = training)
+pr.lm2 <- lm(SalePrice ~ YearBuilt  + KitchenQual + GarageType + ScreenPorch + Fireplaces +
+                     X1stFlrSF + X2ndFlrSF, data = training)
 
 summary(pr.lm2)
-#+ Condition2
 pred <- (predict(pr.lm2, newdata = validation))
 expect <- validation$SalePrice
 
@@ -74,12 +71,10 @@ mse2_valid <- mean((expect-pred)^2)
 mse2_valid
 # [1] 6552447129
 
-pr.lm3 <- lm(SalePrice ~ MSSubClass + LotArea + MSZoning + 
-                     LandSlope + Neighborhood +
-                    OverallQual +
-                     YearRemodAdd + MasVnrArea + BsmtQual +
-                     BsmtExposure  + BsmtFinSF1 + BsmtFinSF2 + BsmtUnfSF +
-                     X1stFlrSF + X2ndFlrSF + ScreenPorch +PoolArea + PoolQC, data = training)
+# Jingyi
+pr.lm3 <- lm(SalePrice ~ YearBuilt + BedroomAbvGr + TotRmsAbvGrd +
+                                            + GarageArea + Neighborhood +
+                                            KitchenQual + OverallCond, data = training)
 
 summary(pr.lm3)
 pred <- (predict(pr.lm3, newdata = validation))
@@ -87,16 +82,11 @@ expect <- validation$SalePrice
 
 mse2_valid2 <- mean((expect-pred)^2)
 mse2_valid2
-# [1] 2228183393
+# [1] 1772643629
 
-# No change in mse!
-
-pr.lm4 <- lm(SalePrice ~ MSSubClass  + MSZoning + 
-                     Neighborhood +
-                     OverallQual +
-                     MasVnrArea + BsmtQual +
-                     BsmtFinSF1 + BsmtFinSF2 + BsmtUnfSF +
-                     X1stFlrSF + X2ndFlrSF , data = training)
+# Alex
+pr.lm4 <- lm(SalePrice ~ MSZoning+ LotArea+ Neighborhood+ YearBuilt+
+                      GarageCars + OverallQual , data=training)
 
 summary(pr.lm4)
 pred <- (predict(pr.lm4, newdata = validation))
@@ -104,5 +94,23 @@ expect <- validation$SalePrice
 
 mse2_valid3 <- mean((expect-pred)^2)
 mse2_valid3
-#[1] 800617307
+#[1] 1716235603
+
 # significant change!
+# training$OverallQual <- as.numeric(training$OverallQual)
+# validation$OverallQual <- as.numeric(validation$OverallQual)
+
+pr.lm5 <- lm(SalePrice ~ OverallQual+ YearBuilt+ GarageCars+ Neighborhood+
+                      LotArea + KitchenQual + ScreenPorch + X1stFlrSF + X2ndFlrSF +
+                     BsmtFinSF1+BsmtFinSF2 + BsmtUnfSF + RoofMatl  + MSZoning + 
+                     PoolArea + GarageType, data=training)
+
+summary(pr.lm5)
+pred <- (predict(pr.lm5, newdata = validation))
+expect <- validation$SalePrice
+
+mse2_valid4 <- mean((expect-pred)^2)
+mse2_valid4
+
+#1102263946
+#1230204676
